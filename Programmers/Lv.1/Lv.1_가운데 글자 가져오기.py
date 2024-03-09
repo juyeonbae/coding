5215aab7cf3024ecd84d217e375fcc8bb8cc3359
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-03-09 23:07:12 +0900

    Create Lv.1_가운데 글자 가져오기.py

---
+++ Programmers/Lv.1/Lv.1_가운데 글자 가져오기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    if len(s) % 2 != 0:
+        answer += s[len(s)//2]
+    else:
+        answer += s[len(s)//2-1] + s[len(s)//2]
+    return answer
+
+
+"""
+조건문 대신 문자열 슬라이스로 축약 가능 
+return s[(len(str)-1)//2 : len(str)//2 + 1] 
+"""
