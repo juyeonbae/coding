7822ddace308c3bf3695a19d90aa474779069e53
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-03-26 20:50:34 +0900

    Delete Programmers/Lv.0/Lv.0_최소직사각형.py

--- Programmers/Lv.0/Lv.0_최소직사각형.py
+++
@@ -1,7 +0,0 @@
-def solution(sizes):
-    big = [] #각 배열마다 큰 값 비교할 리스트
-    small = [] #각 배열마다 작은 값 비교할 리스트
-    for i,j in sizes:
-        big.append(max(i,j))
-        small.append(min(i,j))
-    return max(big) * max(small)
