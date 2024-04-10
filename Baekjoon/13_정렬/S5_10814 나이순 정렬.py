d2666a3d8015203e33f130ffda4cf274225995f9
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-04-10 23:38:44 +0900

    Create S5_10814 나이순 정렬.py

---
+++ Baekjoon/13_정렬/S5_10814 나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for i in range(n):
+    age, name = input().split()
+    arr.append((int(age),i,name))
+
+arr = sorted(arr, key = lambda x: (x[0],x[1]))
+for i in arr:
+    print(i[0],i[2])
