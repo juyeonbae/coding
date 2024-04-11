f2dd77e9946f6e8100a70f8b6b10c2ed4c2fbd93
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-04-12 01:48:59 +0900

    Update S5_13241 최소공배수.py

--- Baekjoon/15_약수, 배수와 소수 2/S5_13241 최소공배수.py
+++ Baekjoon/15_약수, 배수와 소수 2/S5_13241 최소공배수.py
@@ -1,7 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-import math
+#유클리드 호제법
+import sys
+input = sys.stdin.readline
 
+def gcd(x,y):
+    if y > x:
+        x, y = y, x
+    while y > 0:
+        x, y = y, x % y
+    return x
+
+a, b = map(int,input().split())
+gcd = gcd(a,b)
+print(a*b//gcd)
+
+#파이썬 라이브러리 내장 함수 사용 
+import math
 a,b = map(int,input().split())
 print(math.lcm(a,b))
+
+#반복문으로 풀이
+a, b = map(int,input().split())
+for i in range(1, min(a,b)+1):
+    if a % i == 0 and b % i == 0:
+        gcd = i
+
+print(a * b // gcd)
