7f75f1e435a0e92468fcfb80a41386a0cd32f616
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-08-09 13:33:18 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 240 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,27 +1,25 @@
 while True:
-    try:
-        s = input().strip('\n')
-        if s == '.':
-            break
-
-        st, chk = [], True
-        for i in s:
-            if i == '(' or i == '[':
-                st.append(i)
-
-            elif i == ')':
-                if len(st) == 0 or st[-1] != '(':
-                    chk = False
-                else: st.pop()
-            elif i == ']':
-                if len(st) == 0 or st[-1] != '[':
-                    chk = False
-                else: st.pop()
-
-        if chk and not st:
-            print("yes")
-        else:
-            print("no")
-
-    except EOFError:
+    s = input().strip('\n')
+    if s == '.':
         break
+
+    st, chk = [], True
+    for i in s:
+        if i == '(' or i == '[':
+            st.append(i)
+
+        elif i == ')':
+            if len(st) == 0 or st[-1] != '(':
+                chk = False
+            else:
+                st.pop()
+        elif i == ']':
+            if len(st) == 0 or st[-1] != '[':
+                chk = False
+            else:
+                st.pop()
+
+    if chk and not st:
+        print("yes")
+    else:
+        print("no")
