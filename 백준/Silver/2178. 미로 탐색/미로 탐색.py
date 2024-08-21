bb95f1b0742d2af45d5eddba117d130cf1129ffb
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-08-21 09:23:13 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,9 +1,28 @@
 # S1_2178 미로 탐색.py
 from collections import deque
 
+
+# bfs
+def bfs(x, y):
+    Q.append((x, y))
+
+    while Q:
+        x, y = Q.popleft()
+
+        for dir in range(4):
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
+                Q.append((nx, ny))
+
+    return board[n - 1][m - 1]
+
+
+# main
 n, m = map(int, input().split())
 board = [list(map(int, input().strip())) for _ in range(n)]
-dist = [[-1] * m for _ in range(n)]  # 거리 배열 초기화
 
 Q = deque()
 
@@ -11,20 +30,4 @@ Q = deque()
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-Q.append((0, 0))
-dist[0][0] = 0
-
-while Q:
-    x, y = Q.popleft()
-
-    for dir in range(4):
-        nx = x + dx[dir]
-        ny = y + dy[dir]
-
-        if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
-        if dist[nx][ny] >= 0 or board[nx][ny] != 1: continue
-
-        dist[nx][ny] = dist[x][y] + 1
-        Q.append((nx, ny))
-
-print(dist[n-1][m-1] + 1)
+print(bfs(0, 0))
