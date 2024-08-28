db2124dddd400e024efcac2bb6714f05c29d772d
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-08-28 15:22:52 +0900

    [Gold V] Title: 토마토, Time: 2252 ms, Memory: 107596 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+m, n = map(int, input().split())
+board = []
+dist = [[-1] * m for _ in range(n)]
+queue = deque()
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    board.append(row)
+    for j in range(m):
+        if board[i][j] == 1:
+            queue.append((i, j))
+            dist[i][j] = 0
+
+# 방향 벡터
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+while queue:
+    cur_x, cur_y = queue.popleft()
+    for dir in range(4):
+        nx, ny = cur_x + dx[dir], cur_y + dy[dir]
+        if 0 <= nx < n and 0 <= ny < m and dist[nx][ny] == -1 and board[nx][ny] == 0:
+            dist[nx][ny] = dist[cur_x][cur_y] + 1
+            queue.append((nx, ny))
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if dist[i][j] == -1 and board[i][j] == 0:
+            print(-1)
+            exit(0)
+        ans = max(ans, dist[i][j])
+
+print(ans)
