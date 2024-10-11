5de169d6ec85860ad4f10966543aba5e4c2fe062
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-10-11 17:11:44 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,26 +1,15 @@
 def solution(genres, plays):
+    # 딕셔너리 생성 및 장르별 재생 기록 추가
     dct = {}
     for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in dct:
-            dct[g] = []
-        dct[g].append([i, p])
-    # {'classic': [[0, 500], [2, 150], [3, 800]], 'pop': [[1, 600], [4, 2500]]}
+        dct.setdefault(g, []).append([i, p])
 
-    # 각 장르의 리스트를 재생 수(play) 기준으로 내림차순 정렬
+    # 각 장르의 리스트를 재생 수 기준으로 내림차순 정렬
     for g in dct:
-        dct[g].sort(key=lambda x:x[1], reverse=True)
+        dct[g].sort(key=lambda x: x[1], reverse=True)
 
-    # 장르를 총 재생 수 합계 기준으로 내림차순 정렬
+    # 장르별 총 재생 수를 기준으로 딕셔너리 자체도 내림차순 정렬
     dct = dict(sorted(dct.items(), key=lambda x: sum(p for _, p in x[1]), reverse=True))
-    print(dct)
 
-    answer = []
-    for g in dct.values():
-        print(g)
-        if len(g) == 1:
-            answer.append(g[0][0])
-        else:
-            for i in range(2):
-                answer.append(g[i][0])
-
-    return answer
+    # 최대 두 곡씩 인덱스를 추출
+    return [i for g in dct.values() for i, _ in g[:2]]
