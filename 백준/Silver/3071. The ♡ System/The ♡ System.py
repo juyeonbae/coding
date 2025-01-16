64fcbc023f9bf03adba58b128dcfa4ebda790a9b
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2025-01-16 13:04:18 +0900

    [Silver III] Title: The ♡ System, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3071. The ♡ System/The ♡ System.py
+++ 백준/Silver/3071. The ♡ System/The ♡ System.py
@@ -3,31 +3,19 @@ def solution(n):
         return "0"
         
     ans = ''
-    is_negative = n < 0
-    n = abs(n)
     
     while n:
         remainder = n % 3
         n = n // 3
         
-        if remainder == 2:
+        if remainder == 2 or remainder == -2:
             n += 1
+            ans = '-' + ans
+        elif remainder == -1:
             ans = '-' + ans
         else:
             ans = str(remainder) + ans
             
-    if is_negative:
-        # 음수인 경우 각 자리의 부호를 반전
-        new_ans = ''
-        for c in ans:
-            if c == '1':
-                new_ans += '-'
-            elif c == '-':
-                new_ans += '1'
-            else:
-                new_ans += '0'
-        ans = new_ans
-        
     return ans
 
 T = int(input())
