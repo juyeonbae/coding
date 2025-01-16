504ddf93ed412eb79ffbb478c07d0139abfabe7a
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2025-01-16 13:54:17 +0900

    [Silver III] Title: The ♡ System, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3071. The ♡ System/The ♡ System.py
+++ 백준/Silver/3071. The ♡ System/The ♡ System.py
@@ -1,22 +1,19 @@
 def solution(n):
-    if n == 0:
-        return "0"
-        
+    if n == 0: return "0"
+
     ans = ''
-    
     while n:
-        remainder = n % 3
+        r = n % 3
         n = n // 3
-        
-        if remainder == 2 or remainder == -2:
-            n += 1
-            ans = '-' + ans
-        elif remainder == -1:
-            ans = '-' + ans
+
+        if abs(r) == 2 or r == -1:
+            if abs(r) == 2: n += 1
+            ans += '-'
         else:
-            ans = str(remainder) + ans
-            
-    return ans
+            ans += str(r)
+
+    return ans[::-1]
+
 
 T = int(input())
 for _ in range(T):
