316339fc6f7ab40dbc6a5225cd59b27af477b8b5
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2025-02-01 13:40:13 +0900

    [Silver III] Title: 통계학, Time: 440 ms, Memory: 53796 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,25 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-from collections import Counter
-
 N = int(input())
-
 nums = []
+total = 0  # 합계를 위한 변수
+freq_dict = {}  # 빈도수 계산을 위한 딕셔너리
+
+# 입력받으면서 동시에 합계와 빈도수 계산
 for _ in range(N):
-  num = int(input())
-  nums.append(num)
-  
-nums.sort()
-  
-# 산술평균 -> 소수점 첫째자리에서 반올림 
-print(round(sum(nums)/len(nums)))
+    num = int(input())
+    nums.append(num)
+    total += num
+    freq_dict[num] = freq_dict.get(num, 0) + 1
+
+nums.sort()  # 한 번의 정렬
+
+# 산술평균
+print(round(total/N))
 
 # 중앙값
-print(nums[len(nums)//2])
+print(nums[N//2])
 
-# 최빈값 -> 여러 개가 있을 경우 두 번째로 작은 값
-print(Counter(nums).most_common()[1][0] if len(nums) > 1 and Counter(nums).most_common()[0][1] == Counter(nums).most_common()[1][1] else Counter(nums).most_common()[0][0])
+# 최빈값 
+max_freq = max(freq_dict.values())
+mode_vals = sorted([num for num, freq in freq_dict.items() if freq == max_freq])
+print(mode_vals[1] if len(mode_vals) > 1 else mode_vals[0])
 
-# 범위 출력 
+# 범위
 print(nums[-1] - nums[0])
