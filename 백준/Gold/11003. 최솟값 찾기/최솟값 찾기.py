cb5a691939f4e5138c4c1c3dc73fa2a681198797
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-03-03 00:50:39 +0900

    [Gold I] Title: 최솟값 찾기, Time: 3964 ms, Memory: 645716 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,32 +1,22 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
-N, L = map(int, input().split())
-A = list(map(int, input().split()))
-
-dq = deque()
-write = sys.stdout.write
-
-buf = []
-FLUSH = 10000  # 1만개씩 출력
-
-for i, x in enumerate(A):
-    start = i - L + 1
-    while dq and dq[0][1] < start:
-        dq.popleft()
-
-    while dq and dq[-1][0] > x:
-        dq.pop()
-
-    dq.append((x, i))
-
-    buf.append(str(dq[0][0]))
-    if len(buf) == FLUSH:
-        write(" ".join(buf) + " ")
-        buf.clear()
-
-# 남은 것 출력 (마지막은 공백 없이)
-if buf:
-    write(" ".join(buf))
+import sys 
+import collections 
+input = sys.stdin.readline 
+print = sys.stdout.write 
+def main() : 
+    n, l = map(int, input().split()) 
+    d = list(map(int, input().split())) 
+    dq = collections.deque() 
+    
+    for i in range(n) : 
+        while dq and dq[-1] > d[i] : 
+            dq.pop() 
+        
+        dq.append(d[i]) 
+        
+        if i >= l and dq[0] == d[i - l] : 
+            dq.popleft()
+            
+        print(f"{dq[0]} ") 
+    
+if __name__ == "__main__" : 
+    main()
