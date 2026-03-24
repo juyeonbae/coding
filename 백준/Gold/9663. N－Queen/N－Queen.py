fa59b4494d2315ebf16439b0c7f8f0c482b44316
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-03-24 09:22:15 +0900

    [Gold IV] Title: N-Queen, Time: 29728 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -8,30 +8,34 @@ diag_down = [False] * (2 * n - 1) # / 방향
 diag_up = [False] * (2 * n - 1) # \ 방향
 
 
-def count_queens(row):
+def backtrack(row):
+    # 종료 조건 
     if row == n:
         return 1
 
     total = 0 
 
+    # for 선택지 in 가능한 후보들: 
     for col in range(n):
         d_down = row - col + (n-1)
         d_up = row + col
 
         if visited[col] or diag_down[d_down] or diag_up[d_up]:
             continue
-
+            
+        # 선택할 수 있다면 선택 
         visited[col] = True
         diag_down[d_down] = True
         diag_up[d_up] = True
 
-        total += count_queens(row+1)
-
+        total += backtrack(row+1)
+        
+        # 선택 취소
         visited[col] = False
         diag_down[d_down] = False
         diag_up[d_up] = False
 
     return total
 
-print(count_queens(0))
+print(backtrack(0))
         
