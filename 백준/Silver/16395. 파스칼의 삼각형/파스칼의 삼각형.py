4b7ab0fe74e386e30c478b30a4c36faed41d0dd2
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-04-07 11:27:11 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+dp = [[0] * (n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    dp[i][1] = 1
+    dp[i][i] = 1
+
+if n > 2:
+    for i in range(3, n+1):
+        for j in range(1, i):
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j] 
+print(dp[n][k])
