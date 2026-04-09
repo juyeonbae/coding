9c06524b47f77d31ffc3e6db312f8273d0fee0d3
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-04-09 11:03:53 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 340 ms, Memory: 71968 KB, Score: 199 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -0,0 +1,11 @@
+n = int(input())
+MOD = 1000000007
+
+dp = [0] * (n+1)
+if n == 1 or n == 2: print(1)
+if n > 3:
+    dp[1] = dp[2] = 1
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD
+
+    print(dp[n])
