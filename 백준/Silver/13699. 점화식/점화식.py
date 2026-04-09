317e64a91ab5f4180d772a7e3a0b96422700908e
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-04-09 10:32:08 +0900

    [Silver IV] Title: 점화식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [0] * (n+1)
+dp[0] = 1
+if n >= 1:
+    dp[1] = 1
+
+if n > 1:
+    for i in range(2, n+1):
+        for j in range(i):
+            dp[i] += dp[i-j-1] * dp[j]
+
+print(dp[n])
