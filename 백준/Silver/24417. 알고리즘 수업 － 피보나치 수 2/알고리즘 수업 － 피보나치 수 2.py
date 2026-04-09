89dbdd4e8c6daed6cbc54c63e14836fb692b10d6
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-04-09 11:26:24 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.py
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.py
@@ -1,14 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
 MOD = 1000000007
 
-a = b = 1
-for i in range(3, n+1):
-    a, b = b, (a+b) % MOD
+def mat_mul(A, B):
+    n = len(A)
+    C = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for k in range(n):
+            if A[i][k] == 0:
+                continue
+            for j in range(n):
+                C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD
+    return C
 
-ans1 = b 
-ans2 = (n-2) % MOD
+def mat_pow(M, p):
+    n = len(M)
+    # 단위행렬
+    result = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+    while p:
+        if p & 1:
+            result = mat_mul(result, M)
+        M = mat_mul(M, M)
+        p >>= 1
+    return result
+
+n = int(input())
+ans2 = (n - 2) % MOD
+
+base = [[1, 1], [1, 0]]
+result = mat_pow(base, n)
+ans1 = result[0][1]  # fib(n)
 
 print(ans1, ans2)
