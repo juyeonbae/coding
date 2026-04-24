9c7f60f159af2ef760b46943460598a7a201ae79
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-04-24 16:24:56 +0900

    [level 2] Title: N-Queen, Time: 1475.31 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,28 @@
+def solution(n):
+    cols = set()
+    diag1 = set() # row + col
+    diag2 = set() # row - col
+    
+    def dfs(row):
+        if row == n:
+            return 1 
+        
+        count = 0
+        
+        for col in range(n):
+            if col in cols or (row+col) in diag1 or (row-col) in diag2:
+                continue
+                
+            cols.add(col)
+            diag1.add(row+col)
+            diag2.add(row-col)
+            
+            count += dfs(row+1)
+            
+            cols.remove(col)
+            diag1.remove(row+col)
+            diag2.remove(row-col)
+            
+        return count
+    
+    return dfs(0)
