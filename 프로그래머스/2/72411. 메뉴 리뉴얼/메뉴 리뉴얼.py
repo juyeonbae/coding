ca90cd7789505ecb2e64e9c9838c8e2a3fea24ba
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-05-16 14:48:31 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.61 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,26 +1,3 @@
-from itertools import combinations
-
 def solution(orders, course):
     answer = []
-    orders = [list(o) for o in orders]
-    
-    for c in course:
-        d = {}
-        for order in orders:
-            for comb in combinations(order, c):
-                tmp = ''.join(sorted(comb))
-                d[tmp] = d.get(tmp, 0) + 1
-        
-        if not d:
-            continue
-            
-        mx = max(d.values())
-        
-        if mx < 2:
-            continue 
-            
-        for k, v in d.items():
-            if v == mx:
-                answer.append(k)
-                    
-    return sorted(answer)
+    return answer
