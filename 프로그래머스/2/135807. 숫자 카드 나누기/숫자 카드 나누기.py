9b6aee9221362a8d0ba0e6e9f6735940d6f242cc
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-05-19 12:17:07 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 45.17 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,16 +1,3 @@
-from math import gcd
-from functools import reduce
-
 def solution(arrayA, arrayB):
-    gcdA = reduce(gcd, arrayA)
-    gcdB = reduce(gcd, arrayB)
-    
-    answer = 0 
-    
-    if all(b % gcdA != 0 for b in arrayB):
-        answer = max(answer, gcdA)
-        
-    if all(a % gcdB != 0 for a in arrayA):
-        answer = max(answer, gcdB)
-            
+    answer = 0
     return answer
