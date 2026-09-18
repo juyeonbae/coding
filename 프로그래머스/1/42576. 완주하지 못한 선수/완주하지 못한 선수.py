1402b157ed4c465bbf064779c4b3f59443f60d62
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-09-18 18:05:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.44 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,18 @@
-
 def solution(participant, completion):
-    answer, par = '', {}
+    answer = ''
+    
+    d = {}
     for p in participant:
-        if p in par:
-            par[p] += 1
+        if p in d:
+            d[p] += 1 
         else:
-            par[p] = 1
-
+            d[p] = 1
+    
     for c in completion:
-        if c in par:
-            par[c] -= 1
-            if par[c] == 0:
-                del par[c]
-
-    for key in par:
-        answer = key
-
+        if c in d:
+            d[c] -= 1
+    
+    for k, v in d.items():
+        if v == 1:
+            answer = k
     return answer
