3c45fe9dfdc3adc5df61998600f1b2af854c7307
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2026-09-18 18:02:34 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,14 +1,12 @@
 def solution(citations):
+    citations.sort(reverse=True)
+
     answer = 0
-    citations.sort()
-    max_check = 0
-    for h in range(max(citations)+1):
-        cnt = 0
-        for idx in range(len(citations)):
-            if h <= citations[idx]:
-                cnt += 1
-            if cnt == h:
-                answer = cnt
-                max_check = max(max_check,answer)
-    # 테케 11번이 뭐지?
-    return max_check
+
+    for i in range(len(citations)):
+        if citations[i] >= i + 1:
+            answer = i + 1
+        else:
+            break
+
+    return answer
